c58b29bf01b0e97ffc970a50bead464ab4f0c6b5
Phyrunez <ebukajudee302@gmail.com>
2024-04-28 13:46:14 +0100

    worked on register and Login endpoint, using MVC

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+// import cors from 'cors'
+import connectDB from './config/db.js'
+import { errorResponseHandler, invalidPathHandler } from './middleware/errorHandler.js'
+
+import userRoutes from './routes/userRoutes.js'
+
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+let __dirname = path.dirname(__filename)
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
+app.use(express.json())
+
+// app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Server is Running...")
 })
 
-const port = 4040
+app.use("/api/users", userRoutes)
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+app.use(invalidPathHandler);
+// app.use(errorResponseHandler)
+
+const port = 4000 || process.env.PORT
 
 app.listen(port, () => { console.log(`Server listening at port - ${port}`) })
